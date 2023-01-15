63bc575bcb19bd32f85ba39d3c6960a24d387c0b
emericdefay <emericdefay@gmail.com>
2023-01-15 22:27:22 +0100

    GKMAJ001 try

--- GKMAJ001.cob
+++ GKMAJ001.cob
@@ -23,48 +23,32 @@
        ENVIRONMENT DIVISION. 
        INPUT-OUTPUT SECTION. 
        FILE-CONTROL. 
-           SELECT FILEIN-FDNAME
-           ASSIGN TO DYNAMIC FILEIN-NAME
-           FILE STATUS IS WS-FS-FLUX
+      *    FLUX     
+           SELECT FLUX-FILE
+           ASSIGN TO 'FLUX.txt'
+           ORGANIZATION IS LINE SEQUENTIAL.
+      *    REJECT     
+           SELECT REJETS-FILE
+           ASSIGN TO 'REJETS.txt'
            ORGANIZATION IS LINE SEQUENTIAL.
       ******************************************************************
        DATA DIVISION.
       ******************************************************************
        FILE SECTION.
-       FD  FILEIN-FDNAME RECORDING MODE F.
-       01  FILEIN-RECORD.        
-          05  FILLER      PIC X(80).
-
+       FD FLUX-FILE.
+           01 FLUX-ENREG.
+               05 F1-ENREG-00 PIC X(2).
+               05 F1-ENREG-10 PIC X(80).
+       FD REJETS-FILE.
+           01 REJETS-ENREG.
+               05 R-10-F1     PIC X(80).
+               05 R-MOTIF     PIC X(30).
+               05 R-SQLCODE   PIC S9(3) COMP-3.
       ******************************************************************
        WORKING-STORAGE SECTION.
-      /  FILE
-       01  FILEIN-NAME    PIC X(255).
-       01  WS-FS-FLUX     PIC X(02).
-      *    STATUS OK 
-           88 FS-FLUX-OK  VALUE '00'.
-      *    STATUS END FILE 
-           88 FS-FLUX-END VALUE '10'.
-      *    STATUS DDNAME not found 
-           88 FS-FLUX-DDN VALUE '35'.
-      *    STATUS LENGHT or TYPE different 
-           88 FS-FLUX-LEN VALUE '39'.
-       01  WS-VARS.
-      /  Counters
-           05  WS-LUS-00      PIC 9(06).
-           05  WS-LUS-10      PIC 9(06).
-           05  WS-LUS-99      PIC 9(06).
-      /  Operations
-           05  WS-MT-GLOBAL   PIC 9(11)V99.
-      /  Copybook
-       COPY 'CFLUX.cpy'.
-
+       COPY ZFMAJCPT.
       ******************************************************************
        LINKAGE SECTION.
-      /  DYNAMIC FILE 
-       01  FILEIN-DDNAME PIC X(30).
-      /  RETURN CODE
-       01  RC            PIC X(02).
-           88 RC-IS-00   VALUE '00'.
 
       ******************************************************************
       *  Program : Setup, run main routine and exit.
@@ -89,115 +73,68 @@
       *    - Dxxx : Displays
       *    - Cxxx : Calls
       ******************************************************************
-       PROCEDURE DIVISION USING 
-                            FILEIN-DDNAME,
-                            RC.
+       PROCEDURE DIVISION.
+
            PERFORM 1000-Main.
            EXIT PROGRAM.
 
        0000-OFILES.
       ******************************************************************EDEFAY
       *  This routine should open file(s).          
-           MOVE FILEIN-DDNAME TO FILEIN-NAME
-           OPEN INPUT FILEIN-FDNAME
-      
-           IF FS-FLUX-DDN
-               MOVE '01' TO RC
-               PERFORM 1999-FIN
-           END-IF
-           IF FS-FLUX-LEN THEN
-               MOVE '02' TO RC
-               PERFORM 1999-FIN
-           END-IF
+            OPEN INPUT FLUX-FILE
+            OPEN OUTPUT REJETS-FILE
            .
 
        0100-READ-FILEIN.
       ******************************************************************EDEFAY
       *  This routine should read FILEIN file.
-           READ FILEIN-FDNAME
-           INTO FILEIN-RECORD
-           END-READ
+            READ FLUX-FILE
+                AT END SET WS-EOF TO 'Y'
+            END-READ
            .
 
        1000-Main.
       ******************************************************************EDEFAY
       *  This routine should follow the logic of the program purpose.
-           PERFORM 1001-DEBUT
-           IF RC-IS-00 THEN
-               PERFORM UNTIL (FS-FLUX-END)
-                   PERFORM 1500-TRAITEMENT
-               END-PERFORM
-           END-IF
-           PERFORM 1999-FIN
-           EXIT PROGRAM
-           .
-
-       1001-DEBUT.
-      ******************************************************************EDEFAY
-      *  This routine should initialize vars and check if file is empty.
-      *    Initialize vars
-           INITIALIZE WS-VARS
-      *    Setup file
            PERFORM 0000-OFILES
-           IF RC-IS-00 THEN
-               PERFORM 0100-READ-FILEIN
-           END-IF
-           IF FS-FLUX-END THEN
-               MOVE '04' TO RC
-               PERFORM 1999-FIN
-           END-IF
-           .
-
-       1500-TRAITEMENT.
-      ******************************************************************EDEFAY
-      *  This routine should increment WS-LUS-xx vars and price into
-      *  OPER AMOUNT vars. Updating RC if needed.  
-           MOVE FILEIN-RECORD TO F1-ENREG-00
-           EVALUATE TRUE
-               WHEN F1-TYPE-00 = '00'
-                   ADD 1 TO WS-LUS-00
-               WHEN F1-TYPE-00 = '10'
-                   ADD 1 TO WS-LUS-10
-                   ADD F1-MONTANT-OPER TO WS-MT-GLOBAL
-               WHEN F1-TYPE-00 = '99'
-                   ADD 1 TO WS-LUS-99
-                   IF F1-NB-OPERATIONS NOT = WS-LUS-10    THEN
-                       MOVE '05' TO RC
-                       PERFORM 1999-FIN
-                   END-IF
-                   IF F1-MT-GLOBAL     NOT = WS-MT-GLOBAL THEN
-                       MOVE '06' TO RC
-                       PERFORM 1999-FIN
-                   END-IF
-               WHEN OTHER
-                   MOVE '03' TO RC
-                   PERFORM 1999-FIN
-           END-EVALUATE
            PERFORM 0100-READ-FILEIN
+           PERFORM 1100-FLUX-ACTIONS
+           PERFORM 9999-CFILES
            .
 
-       1999-FIN.
+       1001-UPDATE-MAJCPT.
       ******************************************************************EDEFAY
-      *  This routine should end the program, updating RC if needed.
-           IF RC-IS-00 THEN
-      *        No header issue
-               IF WS-LUS-00 = 0 THEN
-                   MOVE '07' TO RC
+      *  This routine should feed ZF-MAJCPT with data from input file
+           MOVE F1-ENREG-10 TO ZF-MAJCPT
+           .
+
+       1100-FLUX-ACTIONS.
+      ******************************************************************EDEFAY
+      *  This routine should read input file to choose the action to do
+           PERFORM UNTIL WS-EOF = 'Y'
+               IF F1-ENREG-00 = '10' THEN
+                   PERFORM 1001-UPDATE-MAJCPT
+                   CALL 'MFMAJCPT' USING ZF-MAJCPT, ZF-RETOUR
+                   IF ZF-CODRET NOT = '00' THEN
+                       PERFORM 1299-WRITE-REJECTS
+                   END-IF
                END-IF
-      *        No footer issue
-               IF WS-LUS-99     = 0
-              AND WS-LUS-00 NOT = 0 THEN
-                   MOVE '08' TO RC
-               END-IF
-           END-IF
-      *    Close file
-           PERFORM 9999-CFILES
-      *    Exit program
-           EXIT PROGRAM
+               PERFORM 0100-READ-FILEIN
+           END-PERFORM
+           .
+
+       1299-WRITE-REJECTS.
+      ******************************************************************EDEFAY
+      *  This routine should write errors in output file
+           MOVE F1-ENREG-10 TO R-10-F1
+           MOVE ZF-LIBRET   TO R-MOTIF
+           MOVE ZF-SQLCODE  TO R-SQLCODE
+           WRITE REJETS-ENREG
            .
 
        9999-CFILES.
       ******************************************************************EDEFAY
       *  This routine should close file(s).
-           CLOSE FILEIN-FDNAME
+            CLOSE FLUX-FILE
+            CLOSE REJETS-FILE
            .
